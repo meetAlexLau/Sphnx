a46d44677ed28d79bdeb155a6ee917163c581bf7
meetAlexLau <alex.lau.1@stonybrook.edu>
2021-10-13 11:53:27 -0400

    barebones client/server setup heroku format

---
+++ server.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let cors = require('cors');
+const path = require("path")
+
+mongoose.connect('mongodb://localhost:27017/SphnxDB', {
+    useNewUrlParser:  true
+}).then(() => {
+    console.log("Local Database Connected");
+}, error => {
+    console.log(error);
+    console.log("Local Database Error Connection");
+});
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+/*
+app.use(express.static(path.resolve(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
+*/
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+//404 Error
