afdb7a6e09ae2e1b4cf80b0a0d0a82410630be05
Syed Affan <saffan98@gmail.com>
2021-10-19 19:17:36 -0400

    question schema

---
+++ models/question-model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Question = new Schema({
+
+    QuestionID: {
+        type: String
+    },
+
+    QuestionText: {
+        type: String
+    },
+
+    QuestionOptions: {
+        type: Map,
+        of: String
+    }
+})
