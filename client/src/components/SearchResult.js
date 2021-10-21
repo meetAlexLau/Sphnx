fa92a43e53b90296b33bce2939e0681326470ef7
meetAlexLau <alex.lau.1@stonybrook.edu>
2021-10-21 19:57:12 -0400

    SearchResults

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -1,35 +1,79 @@
 import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import '../css/SearchResult.css';
 
 export default class SearchResult extends Component{
     constructor(props){
         super(props);
-
+        this.routeChangeHome = this.routeChangeHome.bind(this);
+        this.routeChangeProfile = this.routeChangeProfile.bind(this);
+        this.routeChangePlatform = this.routeChangePlatform.bind(this);
+        this.routeChangeQuiz = this.routeChangeQuiz.bind(this);
     }
     routeChangeHome() {
         //should be  /home/:userid
-        axios.post('/home')
-        .then(res => console.log(res.data));
+        this.props.history.push('/home')
     }
     routeChangeProfile(){
         //should be  /profile/:userid
-        axios.post('/profile')
-        .then(res => console.log(res.data));
+        this.props.history.push('/profile')
+    }
+    routeChangePlatform(){
+        //should be  /profile/:userid
+        this.props.history.push('/platform')
+    }
+    routeChangeQuiz(){
+        //should be  /profile/:userid
+        this.props.history.push('/quiz')
     }
     render(){
         return (
-                <div>
-                <Button onClick={routeChangeLogout} variant="primary">
-                    Login
-                </Button>
-                <Button variant="primary">
-                    Platform Example
-                </Button>
-                <Button onClick={routeChangeProfile} variant="primary">
-                    Profile
-                </Button>
-            </div>
+            <Container fluid className='sky search'>
+                <Container className='medium searchcontainer'>
+                    <Card body className='title'>
+                        Search Results
+                    </Card>
+                    <Row >
+                        <Col md>
+                        <Form.Control size = 'sm' type="text" placeholder="You Searched For: " className='light'/>
+                        </Col>
+                        <Col className='mr-auto'>
+                            <Row>
+                                <Button onClick={this.routeChangeHome} variant='danger' className='ml-auto buttons'>
+                                    Return Home
+                                </Button>
+                            </Row>
+                            <Row>
+                                <Button className='ml-auto buttons'>
+                                    Filter
+                                </Button>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Container className='light searchresult'>
+                        <Row className='mr-auto'>
+                            <Button onClick={this.routeChangePlatform} className ='marginspacing  mr-auto' variant="primary">
+                                Example Platform
+                            </Button>
+                        </Row>
+                        <Row>
+                            <Button onClick={this.routeChangeQuiz} className ='marginspacing' variant="primary">
+                                Example Quiz
+                            </Button>
+                        </Row>
+                        <Row>
+                            <Button onClick={this.routeChangeProfile} className ='marginspacing' variant="primary">
+                                Example Profile
+                            </Button>
+                        </Row>
+                    </Container>
+                </Container>
+            </Container>
         )
     }
 }
