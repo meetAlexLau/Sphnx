93515adf89aabffd860122187b764cf884d81406
Syed Affan <saffan98@gmail.com>
2021-10-21 00:05:03 -0400

    Fixed typo in edit profile component imports

--- client/src/components/EditUserComponent.js
+++ client/src/components/EditUserComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
