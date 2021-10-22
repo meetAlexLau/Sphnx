4c6f73da2ce03393944c6bdb1dede4b1ef30bd64
ManuelSong <sibarnyn@gmail.com>
2021-10-21 22:50:54 -0400

    manuel song commit 5

--- client/src/components/QuizComponent.js
+++ client/src/components/QuizComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Form from 'react-bootstrap/Form';
+//import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+//import Button from 'react-bootstrap/Button';
+//import axios from 'axios';
 
 
 
