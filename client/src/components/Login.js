2e6ac557e5e3749b7330cf7b86a03b38d9c3908f
meetAlexLau <alex.lau.1@stonybrook.edu>
2021-10-28 20:13:56 -0400

    axios bug

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -33,11 +33,7 @@ export default class Login extends Component{
             UserEmail: profile.email,
             UserPoints: 0
         }
-        axios.get('http://localhost:4000/users/'+ '617b24975c98b75e931815fc')
-            .then((res) => {
-                console.log(newUser.UserID)
-                this.routeChange(); //change to home screen
-            })
+        
         axios.post('http://localhost:4000/users/signUp', newUser)
             .then((res) => {
                 console.log(res);
