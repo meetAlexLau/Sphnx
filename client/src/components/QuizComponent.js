6fc653f91dc932c772369ff3719b192c83e01d59
MichaelCarpenzano <mlcarpenzano@gmail.com>
2021-11-04 21:44:50 -0400

    fixed "background" typos

--- client/src/components/QuizComponent.js
+++ client/src/components/QuizComponent.js
@@ -20,7 +20,7 @@ export default class Quiz extends Component {
       isLoggedIn: sessionStorage.getItem('isLoggedIn'),
       quizId: '',
       quizTitle: '',
-      backgourndPic: '',
+      backgroundPic: '',
       questionArray: [{ answerInputArray: [] }],
       answerKeyArray: [],
       numberOfQuestion: '',
@@ -50,7 +50,7 @@ export default class Quiz extends Component {
           this.setState({
             quizId: res.data._id,
             quizTitle: res.data.QuizTitle,
-            backgourndPic: res.data.QuizBackground,
+            backgroundPic: res.data.QuizBackground,
             questionArray: res.data.QuizQuestions,
             answerKeyArray: res.data.QuizAnswerKey,
             numberOfQuestion: res.data.QuizQuestions.length,
@@ -96,13 +96,13 @@ export default class Quiz extends Component {
   //<div className="form-wrapper" style={{ backgroundImage: `url(${quizBackground})` }}>
   //<Button  variant="primary">Next</Button>{' '}
   //backgroundImage: `url(${quizBackground})`
-  // <div style={{ backgroundImage: `url("https://s3-alpha-sig.figma.com/img/793b/aa30/5e39f555720dd1019bc2b4bf12dc7715?Expires=1635724800&Signature=WmJcOUAdHownhxyVuHA4dIaYTNHAm4Wleek0KcE7uh8KR6~Bs~gOINKRRjD-cymZwo4SIctR~o2SFkxKNypOWRqfLUHROGQe~y4relxobiHJLjqDX888ajYiFHuIY~Toim7KvmMbq3LrmUC5rs9HmI4pgXqcvrEtA3f2sHCsOTLlShglgG-pad0Bbx-FnChpyDCqqGu1ZVJyF~wI3jIZml4Hs66Xz7NWUEJeBTeUId8W02dh84FHbqQZucRUwSKjVsitMooSWw1KGmSZu0mlxzqxYVyAKEwtUEvT~Xv1NlfXt82A6RqPSOOykqf0Tdgi8paEY38g353GAMMYxVIIEw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")` }} class="backgorund" >
-  //<div style={{ backgroundImage: `url(${exampleBackground})` }} class="backgorund" >
+  // <div style={{ backgroundImage: `url("https://s3-alpha-sig.figma.com/img/793b/aa30/5e39f555720dd1019bc2b4bf12dc7715?Expires=1635724800&Signature=WmJcOUAdHownhxyVuHA4dIaYTNHAm4Wleek0KcE7uh8KR6~Bs~gOINKRRjD-cymZwo4SIctR~o2SFkxKNypOWRqfLUHROGQe~y4relxobiHJLjqDX888ajYiFHuIY~Toim7KvmMbq3LrmUC5rs9HmI4pgXqcvrEtA3f2sHCsOTLlShglgG-pad0Bbx-FnChpyDCqqGu1ZVJyF~wI3jIZml4Hs66Xz7NWUEJeBTeUId8W02dh84FHbqQZucRUwSKjVsitMooSWw1KGmSZu0mlxzqxYVyAKEwtUEvT~Xv1NlfXt82A6RqPSOOykqf0Tdgi8paEY38g353GAMMYxVIIEw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")` }} class="background" >
+  //<div style={{ backgroundImage: `url(${exampleBackground})` }} class="background" >
   render() {
     return (
 
       <div key={this.state.indexOfQuestion}>
-        <div style={{ backgroundImage: `url(${this.state.backgourndPic})` }} className="backgorund" >
+        <div style={{ backgroundImage: `url(${this.state.backgroundPic})` }} className="background" >
           <div className="quiz-content">
 
             <h1 style={{ textAlign: 'center', fontSize: 20 }}>{this.state.quizTitle}</h1>
