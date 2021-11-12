c11aab152e148196410f6fe36ac512e5c67286ee
Syed Affan <saffan98@gmail.com>
2021-11-12 01:10:10 -0500

    tweak point formula, bonus points if quiz finished within 15 sec

--- client/src/components/QuizComponent.js
+++ client/src/components/QuizComponent.js
@@ -147,7 +147,7 @@ export default class Quiz extends Component {
 
     this.setState({ ResultActive: 1 })
 
-    let pointsScored = ((scoreResult / this.state.numberOfQuestion) * 100) * this.state.numberOfQuestion + (100 - (this.state.totalTime * 10))
+    let pointsScored = ( (((scoreResult / this.state.numberOfQuestion) * 100) * this.state.numberOfQuestion) + ( (15 - (this.state.totalTime / 1000) * 10) ) )
 
     let updatedUser = this.state.oldUser
     updatedUser.UserPoints = updatedUser.UserPoints + pointsScored
