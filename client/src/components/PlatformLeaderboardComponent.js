974f6a3663114e8783c81310278242f6aa8c38cc
ManuelSong <sibarnyn@gmail.com>
2021-11-11 19:25:58 -0500

    leaderboard workig

--- client/src/components/PlatformLeaderboardComponent.js
+++ client/src/components/PlatformLeaderboardComponent.js
@@ -11,12 +11,25 @@ export default class PlatformLeaderboard extends Component {
 
   constructor(props) {
     super(props)
-
+    //this.comparePoints = this.comparePoints.bind(this);
   }
 
+  /*
+  comparePoints(a, b) {
+    return a.point - b.point;
+  }*/
+
+
   render() {
+    let sortedByPointArray=this.props.ScoreBoard
+    
+    //sortedByPointArray.sort(comparePoints)
+    sortedByPointArray.sort((a, b) => (a.point > b.point) ? -1 : 1)
     return (
-          <Container>
+
+
+      <Container>
+        {/*
             <Row>
               <div class="platform-content-row" style={{ backgroundColor: "#D4AF37" }}>
                 <Container>
@@ -87,17 +100,46 @@ export default class PlatformLeaderboard extends Component {
                 </Container>
               </div>
             </Row>
+            */}
+
+        {
+          (sortedByPointArray ? sortedByPointArray : []).map((input, index) => {
+            
+              let color="#000000"
+            
+            if(index==0){
+               color="#D4AF37"
+            }else if(index==1){
+               color="#A8A9AD"
+            }else if(index==2){
+               color="#A97142"
+            }
+
+            return (
+               
+              <Row>
+                <div class="platform-content-row" style={{ backgroundColor: color }}>
+                  <Container>
+                    <Row>
+                      <Col xs lg="2" >
+                        {index+1}.
+                      </Col>
+                      <Col  >
+                        {input.userName}
+                      </Col>
+                      <Col  >
+                        {input.point}
+                      </Col>
+                    </Row>
+                  </Container>
+                </div>
+              </Row>
+            )
+          })
+        }
 
 
-
-
-
-          </Container>
-
-
-
-
-
+      </Container>
 
 
     );
