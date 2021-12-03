7400235f1845f82405ab17c9c634405008e72b4f
Syed Affan <saffan98@gmail.com>
2021-12-02 22:23:37 -0500

    Implemented adding and removing friends from their profile page, display all friends on profile page

--- client/src/components/UserComponent.js
+++ client/src/components/UserComponent.js
@@ -25,17 +25,24 @@ export default class UserComponent extends Component{
             UserPicture: "",
             UserBackgroundPicture: '',
             UserPoints: 0,
-            UserPlatformArray: []
+            UserPlatformArray: [],
+            UserFriendArray: []
         }
 
         this.onClickNewPlatform = this.onClickNewPlatform.bind(this)
         this.onClickPlatform = this.onClickPlatform.bind(this)
+        this.onClickFriend = this.onClickFriend.bind(this)
         this.onClickMyBadge = this.onClickMyBadge.bind(this)
         this.onClickQuiz = this.onClickQuiz.bind(this)
         this.renderPlatforms = this.renderPlatforms.bind(this)
+        this.renderFriends = this.renderFriends.bind(this)
+        this.renderIsFriend = this.renderIsFriend.bind(this)
+        this.friendDisplay = this.friendDisplay.bind(this)
+        this.addFriend = this.addFriend.bind(this)
+
     }
 
-    componentDidMount(){
+    componentDidMount = async() =>{
         console.log(this.props.location.state.isLoggedIn)
         if (this.props.location.state.isLoggedIn == false) {
             this.props.history.push('/')
@@ -53,11 +60,26 @@ export default class UserComponent extends Component{
                         UserSecondaryColor : User.UserSecondaryColor,
                         UserBackgroundPicture : User.UserBackgroundPicture,
                         UserPoints : User.UserPoints,
-                        UserPlatformArray: User.UserPlatformArray
+                        UserPlatformArray: User.UserPlatformArray,
+                        UserFriendArray : User.UserFriendArray
                     }) 
                     this.renderPlatforms();
+                    this.renderFriends();
                 })
         }
+
+        let getUserID = sessionStorage.getItem('UserID');
+        let getFriendID = this.props.match.params.id;
+
+        await axios.get('http://localhost:4000/users/UserID/' + getUserID)
+            .then(res => {
+                let friendFlag = res.data[0].UserFriendArray.find(id => id == getFriendID);
+                if (typeof friendFlag === 'undefined') friendFlag = false;
+                else friendFlag = true;
+                this.setState({
+                    isFriend: friendFlag
+                })
+            })
     }
 
     onClickNewPlatform(){
@@ -74,6 +96,18 @@ export default class UserComponent extends Component{
             pathname:'/platform/' + PlatformID,
             state: {isLoggedIn:true}
             });
+    }
+
+    onClickFriend(friendID){
+
+        console.log('here')
+        sessionStorage.setItem('profileID', friendID)
+        this.props.history.push({
+            pathname:'/profile/' + friendID,
+            state: {isLoggedIn:true}
+
+        })
+
     }
 
     onClickMyBadge(){
@@ -110,12 +144,93 @@ export default class UserComponent extends Component{
             })
         });
     }
-    
+
+    renderFriends(){
+        let f: [number, string] = [];
+        let promises = [];
+
+        for(let i = 0; i < this.state.UserFriendArray.length; i++){
+            let friendID = this.state.UserFriendArray[i];
+            promises.push(
+                axios.get('http://localhost:4000/users/' + friendID)
+                .then(res => {
+                    let object = res.data;
+                    f.push([friendID, object.UserName])
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+            )
+        }
+        Promise.all(promises).then(() => {
+            this.setState({
+                UserFriendArray: f
+            })
+        })
+    }
+
+    friendDisplay(){
+
+        if (this.state.isFriend == true) {
+            document.getElementById('add friend').innerHTML = 'Add friend'
+            alert("Removed " + this.state.UserName + " as a friend!");
+          }
+          else if (this.state.isFriend == false) {
+            document.getElementById('add friend').innerHTML = 'Unfriend'
+            alert("Added " + this.state.UserName + " as a friend!");
+          }
+        
+    }
+
+    renderIsFriend(){
+
+    if (document.getElementById('add friend'))
+      if (this.state.isFriend == true) {
+        document.getElementById('add friend').innerHTML = 'Unfriend';
+      }
+      else if (this.state.isFriend == false) {
+        document.getElementById('add friend').innerHTML = 'Add friend'
+      }
+
+    }
+
+  
     renderNewPlatformButton() {
         if(this.props.match.params.id == sessionStorage.getItem('ID'))
             return <Button onClick={this.onClickNewPlatform} className="profileCard" style={{background: this.state.UserPrimaryColor}}> New Platform </Button>
     }
 
+    addFriend = async () => {
+        this.friendDisplay()
+        let getUserID = sessionStorage.getItem('UserID');
+        let newFriendID = this.props.match.params.id;
+        let user;
+        await axios.get('http://localhost:4000/users/UserID/' + getUserID)
+        .then(res => {
+            user = res.data[0];
+        })
+
+        if(this.state.isFriend == true){
+            let tempFriendArray = user.UserFriendArray;
+            user.UserFriendArray = tempFriendArray.filter(function (value) {
+                console.log(value);
+                return value !== newFriendID;
+            })
+        }
+        else{
+            user.UserFriendArray.push(newFriendID)
+        }
+
+        this.setState({
+            isFriend: !this.state.isFriend
+        })
+
+        await axios.put('http://localhost:4000/users/'+ user._id, user)
+        .then(res => console.log("User friend Arr:", res))
+        .catch(err => console.log("User friend Arr Err:", err))
+    }
+
+    
     render(){
         //<Button onClick={this.onClickPlatform}  className="profileCard" style={{background: this.state.UserPrimaryColor}}>Cowboys</Button>
         //<Button onClick={this.onClickPlatform} className="profileCard" style={{background: this.state.UserPrimaryColor}}>Instruments</Button>
@@ -130,6 +245,19 @@ export default class UserComponent extends Component{
             </Button>
             )
         })
+
+        let friends = this.state.UserFriendArray?.map((friend, i) =>{
+            return(
+                <Button key={i}
+                        onclick={() => this.onClickFriend(friend[0])}
+                        className="profileCard"
+                        style={{background: this.state.UserPrimaryColor}}
+                        >
+                            {friend[1]}
+                        </Button>
+            )
+        })
+
         return(
           <Container fluid style={{ background: this.state.UserPrimaryColor }}>
             <Row>
@@ -191,6 +319,16 @@ export default class UserComponent extends Component{
                     <Button onClick={this.onClickMyBadge} className="profilePageButton" style={{background: this.state.UserPrimaryColor}}>View All Badges</Button>
                     <Link to={{pathname:"/profile/edit", state: {isLoggedIn:true}}} 
                     className="profilePageButton" style={{background: this.state.UserPrimaryColor}}>Edit Profile </Link>
+                    <Button id="add friend"
+                      to={"/"}
+                      onClick={() => this.addFriend()}
+                      className="profilePageButton"
+                      style={{background: this.state.UserPrimaryColor}}
+                    >
+                      {
+                        this.renderIsFriend()
+                      }
+                    </Button>
                 </Col>
                 </Row>
             </Col>
@@ -221,9 +359,12 @@ export default class UserComponent extends Component{
                 style={{background: this.state.UserSecondaryColor}}>
                 <Row>
                     <Col md={12} style={{ margin: "5px 2px" }}>
-                    <div className="profileCard" style={{background: this.state.UserPrimaryColor}}> Recently </div>
-                    <div className="profileCard" style={{background: this.state.UserPrimaryColor}}> Quiz: Racecar Facts</div>
-                    <Button className="profileCard" onClick={this.onClickPlatform}  style={{background: this.state.UserPrimaryColor}}> Platform: Movies</Button>
+                    <div className="profileCard" style={{background: this.state.UserPrimaryColor}}> Friends List </div>
+                    <Row style={{ margin: "8px" }}>
+                    <Col md={12} style={{ margin: "5px 2px" }}>
+                        {friends}
+                    </Col>
+                </Row>
                     </Col>
                 </Row>
                 </div>
