b3d058e0292fefd63f2db9ab6ef53aad071c3b02
Syed Affan <saffan98@gmail.com>
2021-12-13 15:00:28 -0500

    bug fix attempt #2

--- client/src/components/MyBadgeComponent.js
+++ client/src/components/MyBadgeComponent.js
@@ -41,19 +41,21 @@ export default class MyBadge extends Component {
       })
 
     // iterate through and get all badges
+    if(user.UserBadgeArray.length){
     for (let i = 0; i < user.UserBadgeArray.length; i++) {
-      try {
-        await axios.get('/badges/' + user.UserBadgeArray[i])
-          .then(res => {
-            b.push(res.data);
-            this.setState({
-              Badges: this.state.Badges.concat([b[i]])
+        try {
+          await axios.get('/badges/' + user.UserBadgeArray[i])
+            .then(res => {
+              b.push(res.data);
+              this.setState({
+                Badges: this.state.Badges.concat([b[i]])
+              })
             })
-          })
-      } catch (err) {
-        console.log(err);
-      }
+        } catch (err) {
+          console.log(err);
+        }
     }
+  }
     //console.log(this.state.Badges);
     callbackOne();
     callbackTwo();
