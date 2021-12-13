28469765309742bec14f3e25f80e8a3e6abbd2fb
Syed Affan <saffan98@gmail.com>
2021-12-13 13:49:44 -0500

    added post title length limit, set to 12

--- client/src/components/NewPostComponent.js
+++ client/src/components/NewPostComponent.js
@@ -148,7 +148,7 @@ export default class NewPostComponent extends Component {
                     <div class="medium">
                         <Form.Group controlId="Title">
                             <Form.Label>Title:</Form.Label>
-                            <Form.Control type="text" value={this.state.title} onChange={this.onChangePostTitle} />
+                            <Form.Control type="text"  maxLength="12" value={this.state.title} onChange={this.onChangePostTitle} />
                         </Form.Group>
 
                         Select Image:
