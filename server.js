499a27dbcada972bd5370948e52513700639d8d9
meetAlexLau <60982366+meetAlexLau@users.noreply.github.com>
2022-01-03 16:30:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,13 @@ app.use('/badges', badgesRouter);
 
 app.use(express.static(path.resolve(__dirname, "client", "build")))
 
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+})
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
